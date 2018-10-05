0ba159a2235866f0e6706e05debb4df7a80ad06b
Ant0wan <abarthel@student.42.fr>
2018-10-05 12:25:57 +0200

    tail 1st

---
+++ tail/srcs/ft_tail.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <errno.h>
+#include "ft_tail.h"
+
+int	main(void)
+{
+	return (0);
+}
---
+++ tail/srcs/ft_tail.h
@@ -0,0 +1,10 @@
+#ifndef __FT_TAIL_H_
+#	define __FT_TAIL_H_
+
+#define ERRMSG_1 "tail: "
+#define ERRMSG_2 "cannot open \'"
+#define ERRMSG_3 "\' for reading: No such file or directory"
+#define ERRMSG_4 "error reading \'"
+#define ERRMSG_5 "\' Is a directory"
+
+#endif
