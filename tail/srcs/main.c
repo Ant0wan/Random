b6f05eeaca5fb148ed47b86fe451d059648c0558
Ant0wan <abarthel@student.42.fr>
2018-10-09 15:18:29 +0200

    review again

--- tail/srcs/main.c
+++ tail/srcs/main.c
@@ -1,18 +1,16 @@
 #include <unistd.h>
 #include "ft_tail.h"
 
-#include <stdio.h> // DEBUGGING
-
-#define STD_VAL_C_OPTION 10
+#define STD_VAL 10
 
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	lines;
 	int	size;
-	int	c_option_val;
+	int	std_val;
 
-	c_option_val = STD_VAL_C_OPTION;
+	std_val = STD_VAL;
 	i = 1;
 	if (argc == 1)
 		ft_input_term();
@@ -21,7 +19,7 @@ int	main(int argc, char **argv)
 		while (i < argc)
 		{
 			lines = ft_input_lines(argv[i]);
-			size = ft_input_size(argv[i], lines, c_option_val);
+			size = ft_input_size(argv[i], lines, std_val);
 			ft_tail(argv[i], size, ft_input_length(argv[i]));
 			i++;
 		}
