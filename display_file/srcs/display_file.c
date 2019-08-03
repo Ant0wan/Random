9c6ed66d11abe3d4f2d6261b47d00221545d8880
Ant0wan <abarthel@student.42.fr>
2019-08-03 08:06:05 -0400

    update display file, trying to use it to learn performance'

--- display_file/srcs/display_file.c
+++ display_file/srcs/display_file.c
@@ -2,36 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static __inline__ size_t	ft_strlen(char *__restrict__ str)
-{
-	size_t len = 0;;
-
-	while (str[len])
-		++len;
-	return (len);
-}
-
-static __inline__ void		ft_putstr(char *__restrict__ str)
-{
-	size_t	len;
-
-	len = ft_strlen(str);
-	if (fwrite((const void*)str, sizeof(char), len, stdout) != len)
-		perror(NULL);
-}
-
-int				main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	FILE	*stream = NULL;
+	char	buffer[31];
 	size_t	ret = 0;
-	char	buffer[4096];
 	int	i = 1;
 
 	if (argc < 2)
-	{
-		ft_putstr("File name missing.\n");
 		return (EXIT_FAILURE);
-	}
 	while (i < argc)
 	{
 		stream = fopen(argv[i], "r");
@@ -43,7 +22,8 @@ int				main(int argc, char **argv)
 		while ((ret = fread((void*)buffer, sizeof(char), sizeof(buffer) - 1, stream)))
 		{
 			buffer[ret] = '\0';
-			ft_putstr(buffer);
+			if (fwrite((const void*)buffer, sizeof(char), ret, stdout) != ret)
+				perror(NULL);
 		}
 		if (fclose(stream))
 		{
