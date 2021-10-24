b83404e56c1d0791c68eadbb6923f75eb938e7f1
Antoine Barthelemy <antoine@linux.com>
2021-10-24 13:42:58 +0200

    update(scripts): coreos get

--- coreos.sh
+++ coreos.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-wget https://cdimage.ubuntu.com/ubuntu-core/20/stable/current/ubuntu-core-20-armhf+raspi.img.xz -O /tmp/ubuntu-core-20-armhf+raspi.img.xz
-wget https://cdimage.ubuntu.com/ubuntu-core/20/stable/current/SHA256SUMS -O /tmp/coreos-shasums
-diff <(echo $(cat /tmp/coreos-shasums  | grep ubuntu-core-20-armhf+raspi.img.xz | awk '{ print $1 }')) <(echo $(sha256sum /tmp/ubuntu-core-20-armhf+raspi.img.xz | awk '{ print $1 }'))
+set -e
+export BIN='ubuntu-core-20-arm64+raspi.img.xz'
+export URL='https://cdimage.ubuntu.com/ubuntu-core/20/stable/current/'
+export TMP='/tmp/'
+
+wget ${URL}${BIN} -O ${TMP}${BIN}
+wget ${URL}SHA256SUMS -O ${TMP}coreos-shasums
+
+diff <(echo $(cat ${TMP}coreos-shasums  | grep ${BIN} | awk '{ print $1 }')) <(echo $(sha256sum ${TMP}${BIN} | awk '{ print $1 }'))
