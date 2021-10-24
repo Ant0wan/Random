0891d8201ad128444f605e3083dc946f37f0fbf4
Antoine Barthelemy <antoine@linux.com>
2021-10-24 13:50:36 +0200

    update(scripts): sdcard format

--- sdcard.sh
+++ sdcard.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-xzcat /tmp/ubuntu-core-20-armhf+raspi.img.xz | sudo dd of=/dev/mmcblk0 bs=32M
+set -e
+source ./coreos.sh
+export DEVICE='/dev/mmcblk0'
+
+xzcat ${TMP}${BIN} | sudo dd of=${DEVICE} bs=32M
